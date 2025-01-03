31bc6708ee857a9fb767bcef89b8589281979862
altontonn <altontonnalumasa@gmail.com>
2025-01-03 11:59:58 +0300

    Add style to the form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'articles#index'
+  root 'home#home'
   resources :articles, only: [:index, :create, :edit, :new, :show, :destroy]
 end
